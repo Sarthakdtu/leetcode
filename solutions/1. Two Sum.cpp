0c9234c48bc2e95013e22060e158a1ad997e0fe1
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2020-10-09 12:14:28 +0530

    24 ms

--- solutions/1. Two Sum.cpp
+++ solutions/1. Two Sum.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& arr, int k) {
+        // int f=0, s=arr.size()-1;
+        vector<int> ans;
+        map<int, int> freq;
+        for(int i=0;i<arr.size();i++){
+            freq[arr[i]] = i+1;
+        }
+        for(int i=0;i<arr.size();i++){
+            int index = freq[k - arr[i]];
+            if(index>0 && index !=i+1){
+                ans.push_back(i);
+                ans.push_back(index-1);
+                break;
+            }
+        }
+        return ans;
         
     }
 };
