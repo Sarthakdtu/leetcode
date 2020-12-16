0a2648bcfbe02fcbce0ad7292dd0421635e48328
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2020-12-17 00:55:37 +0530

    16 ms

--- solutions/983. Minimum Cost For Tickets.cpp
+++ solutions/983. Minimum Cost For Tickets.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& arr, vector<int>& cost) {
-        vector<int> dp(366);
-        int n = arr.size();
-        dp[arr[n-1]] = min(cost[0], min(cost[1], cost[2]));
-        for(int i=n-2;i>-1;i--){
-            dp[arr[i]] = cost[0] + dp[arr[i+1]];
-            int j;
-            for(j=i+1;j<n;j++)
-                if(arr[i] + 6 < arr[j])
-                    break;
-            if(j==n)
-                dp[arr[i]] = min(dp[arr[i]], cost[1]);
-            else
-                dp[arr[i]] = min(dp[arr[i]], dp[arr[j]] + cost[1]);
+        vector<int> dp(367);
+         int n = arr.size();
+        dp[366] = 0;
+        map<int, int> pres;
+        for(int i = 0;i<n;i++)
+            pres[arr[i]] = 1;
+       
+        // dp[arr[n-1]] = min(cost[0], min(cost[1], cost[2]));
+        for(int i=365;i>-1;i--){
+            dp[i] = dp[i+1];
+            if(pres[i]==1){
+                // cout<<"Going for "<<i<<" selecting ";
+                dp[i] = min(
+                    dp[i+1] + cost[0],
+                    min(
+                        dp[min(i+7, 366)] + cost[1],
+                        dp[min(i+30, 366)] + cost[2]
+                    )
+                );
+                // cout<<dp[i]<<endl;
+            }
+//             dp[arr[i]] = cost[0] + dp[arr[i+1]];
+//             int j;
+//             for(j=i+1;j<n;j++)
+//                 if(arr[i] + 6 < arr[j])
+//                     break;
+//             if(j==n)
+//                 dp[arr[i]] = min(dp[arr[i]], cost[1]);
+//             else
+//                 dp[arr[i]] = min(dp[arr[i]], dp[arr[j]] + cost[1]);
             
-            for(j=i+1;j<n;j++)
-                if(arr[i] + 29 < arr[j])
-                    break;
-            if(j==n)
-                dp[arr[i]] = min(dp[arr[i]], cost[2]);
-            else
-                dp[arr[i]] = min(dp[arr[i]], dp[arr[j]] + cost[2]);
+//             for(j=i+1;j<n;j++)
+//                 if(arr[i] + 29 < arr[j])
+//                     break;
+//             if(j==n)
+//                 dp[arr[i]] = min(dp[arr[i]], cost[2]);
+//             else
+//                 dp[arr[i]] = min(dp[arr[i]], dp[arr[j]] + cost[2]);
         }
-        return dp[arr[0]];
+        return dp[0];
     }
 };
