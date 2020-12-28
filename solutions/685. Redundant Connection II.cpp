2eed277f5836027f648cf308ebd37b35eabf3fc9
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2020-12-28 18:50:49 +0530

    16 ms

--- solutions/685. Redundant Connection II.cpp
+++ solutions/685. Redundant Connection II.cpp
@@ -1,3 +1,27 @@
+vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+    int v = edges.size();
+    int *indegree = new int[v];
+    int *parent = new int[v];
+    int *rank = new int[v];
+    for(int i=0;i<v;i++){
+        parent[i] = i;
+        rank[i] = 1;
+        indegree[i] = -1;
+    }
+    int s, d;
+    bool two_parents = false;
+    pair<int, int> bl1 = {-1, -1}; //black list edge 1
+    pair<int, int> bl2 = {-1, -1}; //black lest edge 2
+​
+    //case 1 : 2 parents
+    //case 2 : just a cycle
+    //case 3 : a cycle and 2 parents
+​
+    //Calculate Indegree
+    for(int i=0;i<v;i++){
+        s = --edges[i][0];
+        d = --edges[i][1];
+        if(indegree[d]==-1)
             indegree[d] = s;
         else{
             two_parents = true;
