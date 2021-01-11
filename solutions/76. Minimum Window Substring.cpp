7cec822ecff1ca03bbe493a15176009246b80a0a
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2021-01-11 21:59:32 +0530

    96 ms

--- solutions/76. Minimum Window Substring.cpp
+++ solutions/76. Minimum Window Substring.cpp
@@ -1,59 +1,39 @@
 class Solution {
 public:
-    bool present(unordered_map<char, int> &freq){
-        for(auto w: freq){
-            if(w.second>0)
-                return false;
-        }
-        return true;
-    }
-    
-    void update(int &cl, int &ml, int &si, int &l, int &r){
-        cl = r- l + 1;
-        if(ml>cl){
-            si = l;
-            ml = cl;
-        }
-        cout<<si;
-    }
-    
     string minWindow(string s, string t) {
-        unordered_map<char, int> freq;
-        int si = -1;
-        int cl = 0;
-        int ml = s.size()+1;
-        for(int i=0;i<t.size();i++)
-            freq[t[i]]++;
+        //explanation https://www.youtube.com/watch?v=e1HlptlipB0&t=1302s
+        int i = 0, j = -1;
+        int start_index = -1, min_len = s.size()+1;
+        unordered_map<char, int> tfreq;
+        unordered_map<char, int> sfreq;
+        int match_count = t.size();
         
-        int l=0, r=0;
-        int prevl = 0, prevr=-1;
-        while(l<s.size() && r<s.size()){
-​
-            freq[s[r]]--;            
-            if(present(freq)){
-                update(cl, ml, si, l, r);
-  
-                freq[s[l]]++;
-                l++;
-                bool go = true;
-                while(l <= r){
-                    if(present(freq)){
-                        update(cl, ml, si, l, r);
-                    }
-                    else
-                        break;
-                    freq[s[l]]++;
-                    l++;
+        for(int i=0;i<t.size();i++){
+            tfreq[t[i]]++;    
+        }
+        int curr_count=0;
+        while(true){
+            //acquire
+            while(i<s.size() && match_count!=curr_count){
+                if(tfreq[s[i]] !=0 && sfreq[s[i]] < tfreq[s[i]])
+                    curr_count++;
+                sfreq[s[i]]++;
+                i++;
+            }
+            if(match_count == curr_count){
+                int curr_len = (i - 1) - (j + 1) + 1;
+                if(curr_len < min_len){
+                    start_index = j+1;
+                    min_len = curr_len;
                 }
             }
-                r++;
-        }
-        string ans = "";
-        if(si != -1){
-            for(int i=0;i<ml;i++){
-                ans  += s[si+ + i];
-            }
-        }
-        return ans;
-    }
-};
+            //release
+            while(j!=i){
+                j++;
+                sfreq[s[j]]--;
+                if(tfreq[s[j]]!=0 && sfreq[s[j]] < tfreq[s[j]])
+                    curr_count--;
+                if(match_count == curr_count){
+                    int curr_len = (i - 1) - (j + 1) + 1;
+                    if(curr_len < min_len){
+                        start_index = j+1;
